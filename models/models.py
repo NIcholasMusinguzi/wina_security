bf9142f8d6b239849af022b9a6673034447ecf1d
derrimugisha <derrimugisha@gmail.com>
2025-09-05 12:51:36 +0300

    removing restrict of creating partener with is_manager

--- models/models.py
+++ models/models.py
@@ -91,47 +91,47 @@ class ProductTemplate(models.Model):
     
     _inherit = 'product.template'
 
-    # @api.model
-    # def create(self, vals):
-    #     if self.env.user.is_manager_access != True:
-    #         raise UserError(
-    #             _('Sorry, you are not allowed to create new product.'),
-    #         )
-    #     else:
-    #         return super(ProductTemplate, self).create(vals)
+    @api.model
+    def create(self, vals):
+        if self.env.user.is_manager_access != True:
+            raise UserError(
+                _('Sorry, you are not allowed to create new product.'),
+            )
+        else:
+            return super(ProductTemplate, self).create(vals)
     
     
-    # def write(self, vals):
-    #     # Check if the user has is_manager_access=True
-    #     if not self.env.user.is_manager_access:
-    #         # Check if name or default_code is being modified
-    #         restricted_fields = ['name', 'default_code']
-    #         modified_restricted_fields = [field for field in restricted_fields if field in vals]
-    #         if modified_restricted_fields:
-    #             raise UserError(
-    #                 _('Sorry, you are not allowed to edit the following fields: %s.') % ', '.join(modified_restricted_fields)
-    #             )
-    #     return super(ProductTemplate, self).write(vals)
+    def write(self, vals):
+        # Check if the user has is_manager_access=True
+        if not self.env.user.is_manager_access:
+            # Check if name or default_code is being modified
+            restricted_fields = ['name', 'default_code']
+            modified_restricted_fields = [field for field in restricted_fields if field in vals]
+            if modified_restricted_fields:
+                raise UserError(
+                    _('Sorry, you are not allowed to edit the following fields: %s.') % ', '.join(modified_restricted_fields)
+                )
+        return super(ProductTemplate, self).write(vals)
 
 class Product(models.Model):
     
     _inherit = 'product.product'
  
-    # @api.model
-    # def create(self, vals):
-    #     if not self.env.user.is_manager_access:
-    #         raise UserError(_('Sorry, you are not allowed to create new product.'))
-    #     return super().create(vals)
+    @api.model
+    def create(self, vals):
+        if not self.env.user.is_manager_access:
+            raise UserError(_('Sorry, you are not allowed to create new product.'))
+        return super().create(vals)
 
-    # def write(self, vals):
-    #     if not self.env.user.is_manager_access:
-    #         restricted_fields = ['name', 'default_code']
-    #         modified_restricted_fields = [field for field in restricted_fields if field in vals]
-    #         if modified_restricted_fields:
-    #             raise UserError(
-    #                 _('Sorry, you are not allowed to edit the following fields: %s.') % ', '.join(modified_restricted_fields)
-    #             )
-    #     return super().write(vals)
+    def write(self, vals):
+        if not self.env.user.is_manager_access:
+            restricted_fields = ['name', 'default_code']
+            modified_restricted_fields = [field for field in restricted_fields if field in vals]
+            if modified_restricted_fields:
+                raise UserError(
+                    _('Sorry, you are not allowed to edit the following fields: %s.') % ', '.join(modified_restricted_fields)
+                )
+        return super().write(vals)
     
 
     
